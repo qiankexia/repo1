3620143f01fed3c058c9218a95638c208b351ea8
gongjunjie <gongjunjie@trendit.cn>
2021-06-02 19:01:45 +0800

    将工程添加到版本库

---
+++ project/func.c
@@ -0,0 +1,9 @@
+#include "func.h"
+#include <stdio.h>
+
+extern int aa;
+
+void func()
+{
+	printf("aa=%d void func()\n",aa);
+}
---
+++ project/mian.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "func.h"
+
+static int aa = 10;
+
+int main()
+{
+	printf("main.c\n");
+	func();
+	return 0;
+}
